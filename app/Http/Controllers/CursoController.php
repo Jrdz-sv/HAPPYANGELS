c240174952c0e2c2681415a4bc1f5e17b2b6ed51
JonathanRdz <rodriguezjonathan0005@gmail.com>
2023-06-03 17:08:05 -0600

    re-routing cruds for registros

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Curso;
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Retrieve all cursos from the database
+        $cursos = Curso::all();
+
+        // Pass the cursos to the view
+        return view('cursos.index', compact('cursos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Curso $curso)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Curso $curso)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Curso $curso)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Curso $curso)
+    {
+        //
+    }
+}
