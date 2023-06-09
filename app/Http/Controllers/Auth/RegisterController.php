53640ed28f0c088f4d146d028c424fe2bb0a934c
Manrique17 <1294185@clases.edu.sv>
2023-06-09 16:50:51 -0600

    Migraciones

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
-            'rol' => 1,
+            'rol' => 0,
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
